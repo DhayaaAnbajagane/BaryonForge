0e21e58b6d5cce57378eff45bea81b53710d0b13
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2025-11-26 19:49:04 -0500

    fix import

--- BaryonForge/Profiles/__init__.py
+++ BaryonForge/Profiles/__init__.py
@@ -2,4 +2,4 @@ from ..Profiles.Schneider19 import *
 from ..Profiles.Thermodynamic import *
 from ..Profiles.BaryonCorrection import *
 from ..Profiles.Base import *
-from . import Schneider19, Arico20, Mead20, Battaglia, Base, misc, HOD
+from . import Schneider19, Arico20, Mead20, Battaglia, Base, misc
